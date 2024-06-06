75e543af82cd1072786e6f9af0cfdd411aa44918
alpmumcuu <alp.mumcu@stu.ocsb.ca>
2024-06-06 14:11:05 -0400

    aa

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /**
  * This file will get loaded by index.html
  */
+
+
+// Add an event listener to the toggle button
+document.getElementById('toggle-button').addEventListener('click', function() {
+  // Toggle the dark mode class
+  document.body.classList.toggle('dark-mode');
+
+});
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+if (prefersDarkMode.matches) {
+  document.body.classList.add('dark-mode');
+}
+
+  
+  // Call the function to animate the text
+  animateText();
